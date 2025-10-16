179f1770158f5f8cf3764988f1f3508ce9aa29a0
1059-25-HTL3R <1059@htl.rennweg.at>
2025-10-16 18:18:01 +0200

    makierung bei (2*pi)/3 added + bewschriftung für das ding

--- UE_03/plot1.py
+++ UE_03/plot1.py
@@ -28,6 +28,27 @@ plt.xticks([-np.pi, -np.pi/2, 0, np.pi/2, np.pi],
 plt.yticks([-1,1],
         [r'$-1$', r'$+1$'])
 
+#makierung bei sin((2*pi)/2)
+wert = (2 * np.pi) / 3
+plt.plot([wert, wert], [0, np.cos(wert)], color='blue', linewidth=2.5, linestyle="--")
+plt.plot([wert, wert], [0, np.sin(wert)], color='red', linewidth=2.5, linestyle="--")
+
+#punktbeschriftung
+plt.annotate(r'$\sin(\frac{2\pi}{3})$',
+             xy=(wert, np.sin(wert)), xycoords='data',
+             xytext=(+10, +30), textcoords='offset points', fontsize=16,
+             arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
+
+plt.annotate(r'$\cos(\frac{2\pi}{3})$',
+             xy=(wert, np.cos(wert)), xycoords='data',
+             xytext=(+10, +30), textcoords='offset points', fontsize=16,
+             arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
+
+# Beschriftung hervorheben
+for label in ax.get_xticklabels() + ax.get_yticklabels():
+    label.set_fontsize(16)
+    label.set_bbox(dict(facecolor='white', edgecolor='None', alpha=0.65))
+
 
 # Layout
 plt.tight_layout()
