d6b05da7f80fc2846e33407d12f3c8c10179ecf4
1059-25-HTL3R <1059@htl.rennweg.at>
2025-10-16 18:17:02 +0200

    achsen und achsenbeschriftung geadded

--- UE_03/plot1.py
+++ UE_03/plot1.py
@@ -16,6 +16,18 @@ plt.plot(x, y_sin, color="red", linewidth=2, label="Sinus")
 plt.title("Zwettler")
 plt.legend(loc="upper left")
 
+#achsen
+ax.spines['left'].set_position('center')   # y-Achse durch 0
+ax.spines['bottom'].set_position('center') # x-Achse durch 0
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+
+#achsenbeschriftung
+plt.xticks([-np.pi, -np.pi/2, 0, np.pi/2, np.pi],
+       [r'$-\pi$', r'$-\pi/2$', r'$0$', r'$+\pi/2$', r'$+\pi$'])
+plt.yticks([-1,1],
+        [r'$-1$', r'$+1$'])
+
 
 # Layout
 plt.tight_layout()
