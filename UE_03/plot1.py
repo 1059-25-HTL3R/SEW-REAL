efcdb0e0b703bde623a663a240ce0b333c67273a
Benjamin Zwettler <1059@htl.rennweg.at>
2025-10-19 15:42:43 +0200

    no numpy

--- UE_03/plot1.py
+++ UE_03/plot1.py
@@ -1,10 +1,13 @@
-import numpy as np
+#import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 # Daten
-x = np.linspace(-np.pi , np.pi, 800)
-y_sin = np.sin(x)
-y_cos = np.cos(x)
+x = [(-math.pi) +
+     i * (2 * math.pi / 799)
+     for i in range(800)]
+y_sin = [math.sin(x) for x in x]
+y_cos = [math.cos(x) for x in x]
 
 fig, ax = plt.subplots()
 
@@ -23,24 +26,24 @@ ax.spines['right'].set_color('none')
 ax.spines['top'].set_color('none')
 
 #achsenbeschriftung
-plt.xticks([-np.pi, -np.pi/2, 0, np.pi/2, np.pi],
+plt.xticks([-math.pi, -math.pi/2, 0, math.pi/2, math.pi],
        [r'$-\pi$', r'$-\pi/2$', r'$0$', r'$+\pi/2$', r'$+\pi$'])
 plt.yticks([-1,1],
         [r'$-1$', r'$+1$'])
 
 #makierung bei sin((2*pi)/2)
-wert = (2 * np.pi) / 3
-plt.plot([wert, wert], [0, np.cos(wert)], color='blue', linewidth=2.5, linestyle="--")
-plt.plot([wert, wert], [0, np.sin(wert)], color='red', linewidth=2.5, linestyle="--")
+wert = (2 * math.pi) / 3
+plt.plot([wert, wert], [0, math.cos(wert)], color='blue', linewidth=2.5, linestyle="--")
+plt.plot([wert, wert], [0, math.sin(wert)], color='red', linewidth=2.5, linestyle="--")
 
 #punktbeschriftung
 plt.annotate(r'$\sin(\frac{2\pi}{3})$',
-             xy=(wert, np.sin(wert)), xycoords='data',
+             xy=(wert, math.sin(wert)), xycoords='data',
              xytext=(+10, +30), textcoords='offset points', fontsize=16,
              arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
 
 plt.annotate(r'$\cos(\frac{2\pi}{3})$',
-             xy=(wert, np.cos(wert)), xycoords='data',
+             xy=(wert, math.cos(wert)), xycoords='data',
              xytext=(+10, +30), textcoords='offset points', fontsize=16,
              arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
 
