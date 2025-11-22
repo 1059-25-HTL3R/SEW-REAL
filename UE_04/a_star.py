965c57c77bd405d3a19b7a280bb021ee1f0be764
1059-25-HTL3R <1059@htl.rennweg.at>
2025-11-22 19:31:46 +0100

    comments

--- UE_04/a_star.py
+++ UE_04/a_star.py
@@ -1,32 +1,51 @@
 import heapq
 from typing import List
 
+class Node:
+    def __init__(self, x=None, y=None):
+        self.parent: Node = None
+        self.x = x
+        self.y = y
+        self.f = float('inf')
+        self.g = float('inf')
+        self.h = 0
+
+    def __lt__(self, other):
+        return self.f < other.f  # für heapq
+    def __repr__(self):
+        return f'({self.x}, {self.y})'
 
 def a_star(maze_lines: List[str]):
 
+    #simple direction list
     directions = [(-1,0),(1,0),(0,-1),(0,1)]
 
+    #gets all neighbores / children from a specific node
     def get_children(node: Node):
         children = []
         for dx, dy in directions:
-            nx, ny = node.x + dx, node.y + dy
+            nx, ny = node.x + dx, node.y + dy #<- schreibweise von chat
             if 0 <= ny < len(maze_lines) and 0 <= nx < len(maze_lines[0]):
                 if maze_lines[ny][nx] != '#':  # '#' = Wand
                     child = Node(nx, ny)
                     children.append(child)
         return children
 
+    #calculates the manhatten h value
     def get_h(a: Node, b: Node):
         return abs(a.x - b.x) + abs(a.y - b.y)
 
+    # recursivly reconstructs the path
     def get_path(node: Node):
         path = []
         while node:
             path.insert(0, node)
             node = node.parent
         return path
-
-    start = escape = None
+    # initialize the start and end node
+    start = None
+    escape = None
+    # find the coordinates of the start and end node
     for y, line in enumerate(maze_lines):
         for x, c in enumerate(line):
             if c == 'x':
@@ -36,18 +55,21 @@ def a_star(maze_lines: List[str]):
             elif c == 'A':
                 escape = Node(x, y)
 
+    # if one of the start / end nodes werent found raise an error
     if not start or not escape:
         raise ValueError("Start oder Escape fehlt")
 
+    # initialize the open and closeed dicts / list
     open_heap = []
     open_dict = {}
     closed_dict = {}
 
+    #push start node on open list
     heapq.heappush(open_heap, (start.f, start))
     open_dict[(start.x, start.y)] = start
 
     while open_heap:
-        _, current = heapq.heappop(open_heap)
+        current_f, current = heapq.heappop(open_heap)
         del open_dict[(current.x, current.y)]
         closed_dict[(current.x, current.y)] = current
 
@@ -82,16 +104,3 @@ if __name__ == "__main__":
     main()
 
 
-class Node:
-    def __init__(self, x=None, y=None):
-        self.parent: Node = None
-        self.x = x
-        self.y = y
-        self.f = float('inf')
-        self.g = float('inf')
-        self.h = 0
-
-    def __lt__(self, other):
-        return self.f < other.f  # für heapq
-    def __repr__(self):
-        return f'({self.x}, {self.y})'
